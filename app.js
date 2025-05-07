0c2b96653c97556865dba26c1e1a1690e5fa14f4
programme0419 <kinchinbrian@gmail.com>
2025-05-07 07:16:10 -0600

    create

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '${process.env.PORT}', // Your frontend URL
+    origin: process.env.FRONTEND_URL, // Your frontend URL
     credentials: true, // Important for cookies/sessions
   })
 );
